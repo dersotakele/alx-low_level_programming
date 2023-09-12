2a27045ba494d56e7aadb68ffe063fdc05fb05bb
dersotakele <dersotakele@gmail.com>
2023-09-12 05:05:38 -0700

    8-print_base16;

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char alpha;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
