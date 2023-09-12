98341cfc9315f1574736c8138a68da454a35cda6
dersotakele <dersotakele@gmail.com>
2023-09-12 05:42:08 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, y;
+
+	for (n = '0'; y < '9'; n++)
+	{
+		for (y = n + 1; y <= '9'; y++)
+		{
+			if (n != y)
+			{
+				putchar(n);
+				putchar(y);
+				if (n == '8' && y == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
