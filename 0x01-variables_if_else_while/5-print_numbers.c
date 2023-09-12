9d789548928ed924d2b896e78094e96c1e6c4112
dersotakele <dersotakele@gmail.com>
2023-09-12 04:26:04 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
