fc68d69f5aac8aad9a91c6f83dacd4aa80387f73
dersotakele <dersotakele@gmail.com>
2023-09-15 05:26:30 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit-function to search for digit
+ * @c:parameter to check for the digit
+ * Return:1
+ * int data type
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
