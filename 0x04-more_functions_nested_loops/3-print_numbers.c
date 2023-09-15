5d4a325a7d659b34b73531ac98dd0598a6b0893c
dersotakele <dersotakele@gmail.com>
2023-09-15 05:37:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers-function to print basic integers
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
