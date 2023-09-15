7c5d082ba61357292a93b77dba6f4422989ac25a
dersotakele <dersotakele@gmail.com>
2023-09-15 05:30:49 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul-function to multiply two integers
+ * @x:first integer
+ * @y:second integer
+ * Return:multiplcation
+ */
+
+int mul(int n, int m)
+{
+	int multproduct;
+
+	multproduct = n * m;
+	return (multproduct);
+}
