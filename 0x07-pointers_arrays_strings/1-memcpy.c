9f0674f087842d0fb2551c7f821e66ab0210b6be
dersotakele <dersotakele@gmail.com>
2023-09-25 05:59:28 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,14 +10,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	 char *original_dest = dest;
+	char *original_dest = dest;
 
-	     while (n > 0)
-	     {
-		     *dest = *src; 
-		     dest++;
-		     src++;
-		     n--;
-	     }
-	     return original_dest;
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	return (original_dest);
 }
