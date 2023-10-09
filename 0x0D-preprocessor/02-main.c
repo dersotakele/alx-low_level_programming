e87a3e1a63ece7251cf03ff2b9e84f4af93e9adf
dersotakele <dersotakele@gmail.com>
2023-10-09 03:59:57 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Compiled from file:%s\n", __FILE__);
+	return (0);
+}
