78826d7c6c3d67f3d898965bdcaef02a350da095
dersotakele <dersotakele@gmail.com>
2023-10-24 04:41:32 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
 
 	while (*head)
 	{
-		tmp = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = tmp;
+		*head = temp;
 	}
+
+	*head = NULL;
 }
