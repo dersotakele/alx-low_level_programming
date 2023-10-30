a17118e3a1faeb7cfd4bf183f027818b2bd9a58c
dersotakele <dersotakele@gmail.com>
2023-10-30 05:06:55 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: content writed in the file.
+ * @text_cont: content writed in the file.
  *
  * Return: 1 if it success. -1 if it fails.
  */
