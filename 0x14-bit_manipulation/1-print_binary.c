e4780d0291efbd593b6bfc87e5caa9e177676484
dersotakele <dersotakele@gmailcom>
2023-11-23 04:00:38 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,23 +11,23 @@ void print_binary(unsigned long int n)
 	unsigned long int temp;
 	int shift;
 
-        if (n == 0)
-        {
-                printf("0");
-                return;
-        }
-        temp = n;
-        shift = 0;
-        while ((temp >>= 1) > 0)
-        {
-                shift++;
-        }
-        while (shift >= 0)
-        {
-                if ((n >> shift) & 1)
-                        printf("1");
-                else
-                        printf("0");
-                shift--;
-        }
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	temp = n;
+	shift = 0;
+	while ((temp >>= 1) > 0)
+	{
+		shift++;
+	}
+	while (shift >= 0)
+	{
+		if ((n >> shift) & 1)
+			printf("1");
+		else
+			printf("0");
+		shift--;
+	}
 }
