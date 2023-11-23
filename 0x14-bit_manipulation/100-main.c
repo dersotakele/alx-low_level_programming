90908debac66d910ffab690886a398dd58bf0810
dersotakele <dersotakele@gmail.com>
2023-11-23 05:31:07 -0800

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,19 @@
+julien@ubuntu:~/0x14. Binary$ cat 100-main.c
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    int n;
+
+    n = get_endianness();
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return (0);
+}
